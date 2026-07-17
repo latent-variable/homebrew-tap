f6bbb0e8e73987369516727c2b96ef2b104785a3
Lino Valdovinos <linoa7x@hotmail.com>
2026-07-16 19:30:48 -0700

    yap 0.8.1

--- Casks/yap.rb
+++ Casks/yap.rb
@@ -1,6 +1,6 @@
 cask "yap" do
-  version "0.8.0"
-  sha256 "d6bbff30d40860a7646ca7d535951b90745dbb64fde4e38949ba352ff51b33eb"
+  version "0.8.1"
+  sha256 "03c15e5cbdacc8fa04ca03db1d9433dc416ee5bdd36dcd4582a221162159c9c6"
 
   url "https://github.com/latent-variable/Yap/releases/download/v#{version}/Yap-#{version}.dmg"
   name "Yap"
